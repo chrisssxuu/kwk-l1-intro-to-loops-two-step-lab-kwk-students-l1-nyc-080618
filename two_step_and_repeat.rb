8890c20a1ea0f89ba60e2aa3c91c9f64f53de7fa
chrisssxuu <chris01.xu@gmail.com>
2018-08-08 14:37:30 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -22,13 +22,15 @@ def a_few_more_steps
     sleep(0.5)
     puts "Right foot back"
     sleep(0.5)
-puts "Stop"
-sleep(1)
+    puts "Stop"
+    sleep(1)
     puts "Right foot steps right and back"
     sleep (0.5)
     puts "Left foot crosses over right"
     sleep (0.5)
     puts "Right foot steps right"
+    sleep (0.5)
+    puts "Turn"
     sleep (1)
   end
 end
